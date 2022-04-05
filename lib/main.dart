ef3234152e97fe85ea17455c52e63f1ab78d41fb
Dogukan Balaman <dodikbalaman@hotmail.com>
2022-04-05 20:44:33 +0300

    Bozuk Veri Ekleme

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,53 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unused_element
 
-import 'package:flutter/material.dart'; // Scaffold, Buton ve AppBar için gerekli kaynaklar.
+import 'package:flutter/material.dart';
+// Scaffold, Buton ve AppBar için gerekli kaynaklar.
 //Flutter bu paket ile geliyor. Genel olarak geliştiriciler işlerini kolaylaştırmak ve hızlandırmak için paketler kullanırlar. bkz. https://pub.dev/
 
-void main() => runApp(
-      MaterialApp(home: ToDo()), //Material'dan gelen 1. class
-    ); //MyApp sonuç verirse main'i çalıştır, vermez ise çalıştırma
+//void main() => runApp(
+//    MaterialApp(home: Todo()), //Material'dan gelen 1. class
+//); //MyApp verirse main'i çalıştır, vermez ise çalıştırma
+void main() {
+  runApp(App());
+}
 
-class ToDo extends StatelessWidget {
-  //StatelessWidget sadece AppBar gibi yeri asla değişmeyecek yerlerde kullanılır. Bizim listemiz sürekli değişir.
+class App extends StatelessWidget {
+//StatelessWidget sadece AppBar gibi yeri asla değişmeyecek yerlerde kullanılır. Bizim listemiz sürekli değişir.
 //Telefonun modeline göre ekranın üst tarafı kamera yüzünden değişebilir fakat bunu şu anlık ayarlamayacağım.
   @override
   Widget build(BuildContext context) {
     //Flutter Widget'ları uygulamanın o anki durumuna göre nasıl gözükeceğini belirler.
-    //Widget iskeleti
-    // uygulamanın iskeleti
     return MaterialApp(
         //Tüm kod MaterialApp'ın içerdiği bir widget gibi düşünülebilir.
-        //Material'dan gelen 2. class
-        home: Scaffold(
-      //Scaffold manuel olarak ayarlama yapmamak için yardımcı olur.
-      //Material'dan gelen 3. class
-      appBar: AppBar(
-        //Başlık mantığı ekranın üstüne yazı eklemeye yarar sağlar.
-        //Material'dan gelen 4. class
-        title: Text('Yapılacaklar Listesi'), //1. Widget
-      ),
-    ));
+        //Material'dan gelen class
+        title: 'Yapılacaklar Listesi',
+        home: TodoList());
   }
 }
 
-final List<String> _todoList = <String>[];
-// text yazma yeri
-final TextEditingController _textFieldController = TextEditingController();
-
-void setState(Null Function() param0) {} //setState fix'i
-
-// Listeye veri ekleme
-void _addTodoItem(String baslik) {
-  //  a set state will notify the app that the state has changed
-  setState(() {
-    _todoList.add(baslik);
-  });
-  // the text field is cleared once the item is added to list
-  _textFieldController.clear();
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
 }
 
-//listeyi doldurma
-Widget _buildTodoItem(String baslik) {
-  return ListTile(title: Text(baslik));
+class _TodoListState extends State<TodoList> {
+  final List<String> _todoList = <String>[];
+  // text yazma yeri
+  final TextEditingController _textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Uygulama düzeni
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Yapılacaklar Listesi'),
+      ),
+      body: ListView(children: _getItems()),
+      // listeye ekleme yapma
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
 }
